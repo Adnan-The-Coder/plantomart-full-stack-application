aeb7106cc1cd1aaf943a8541f8f84f1ac121f6b3
Adnan <adnanali23189@gmail.com>
2025-09-07 21:29:19 +0530

    update postcss.config.mjs

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,11 +1,7 @@
-// ✅ ESM syntax
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-/** @type {import('postcss').Postcss} */
+/** @type {import('postcss-load-config').Config} */
 export default {
-  plugins: {
-    tailwindcss,
-    autoprefixer,
-  },
+  plugins: [tailwindcss, autoprefixer],
 };
