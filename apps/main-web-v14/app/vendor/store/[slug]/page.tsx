b7250e9d2b20d0d64a2e0d78696693da420cf343
Adnan <adnanali23189@gmail.com>
2025-09-09 03:17:31 +0530

    update vendor store page dynamic routing segments to v15

--- apps/main-web-v14/app/vendor/store/[slug]/page.tsx
+++ apps/main-web-v14/app/vendor/store/[slug]/page.tsx
@@ -2,8 +2,17 @@ import CompletePage from "@/components/vendor-store/CompletePage";
 
 export const runtime = 'edge';
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } =  params
+// export default async function Page({ params }: { params: { slug: string } }) {
+//   const { slug } =  params
 
+//   return <CompletePage slug={slug} />
+// }
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
   return <CompletePage slug={slug} />
 }
