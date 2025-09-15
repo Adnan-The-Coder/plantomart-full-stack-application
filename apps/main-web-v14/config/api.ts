a7c8a46c0c36c377cafc449b0190f6aeee5adaf4
Adnan <adnanali23189@gmail.com>
2025-09-15 22:09:23 +0530

    update backend API url with new deployed version

--- apps/main-web-v14/config/api.ts
+++ apps/main-web-v14/config/api.ts
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = process.env.BACKEND_API_BASE_URL || 'http://127.0.0.1:8787';
-export const API_BASE_URL = 'https://backend-server.developer-plantomart.workers.dev';
+export const API_BASE_URL = 'https://cf-server.plantomart.workers.dev';
 
 export const API_ENDPOINTS = {
     // userProfile Endpoints 
